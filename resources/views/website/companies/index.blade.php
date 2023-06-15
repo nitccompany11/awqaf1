2bae8e3a1dfe8bc50ee6ba2fca79ca7296b82202
Ahmed Taher <xzcmido@gmail.com>
2023-06-15 15:15:15 +0300

    Set Width, Height for Company Images

--- resources/views/website/companies/index.blade.php
+++ resources/views/website/companies/index.blade.php
@@ -17,7 +17,7 @@
                     <a href="{{ route('get.company', [$company->id, $company->getUrlTitle()]) }}">
                         <div class="d-flex justify-content-center flex-column align-items-center">
                             <div class="company-img w-50 h-50 d-flex justify-content-center align-items-center mb-4">
-                                <img src="{{ $company->image }}" alt="{{ $company['name_' . getLang()] }}" alt="company 3" class="img-fluid">
+                                <img src="{{ $company->image }}" alt="{{ $company['name_' . getLang()] }}" alt="company 3" class="img-fluid" style="width: 100%;height: 125px;">
                             </div>
                             <div class="title-group w-100 text-center py-4">
                                 <h5>
