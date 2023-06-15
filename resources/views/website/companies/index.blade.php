64931ecb55d9f943ac47728c8b15bce4027c0ee0
Ahmed Taher <xzcmido@gmail.com>
2023-06-15 15:00:21 +0300

    Update Company View

--- resources/views/website/companies/index.blade.php
+++ resources/views/website/companies/index.blade.php
@@ -13,18 +13,20 @@
             </div>
             <div class="row company-list">
                 @foreach($companies as $company)
-                <div class="col-12 col-md-4 item d-flex justify-content-center flex-column align-items-center">
+                <div class="col-12 col-md-4 item">
                     <a href="{{ route('get.company', [$company->id, $company->getUrlTitle()]) }}">
-                        <div class="company-img w-50 h-50 d-flex justify-content-center align-items-center mb-4">
-                            <img src="{{ $company->image }}" alt="{{ $company['name_' . getLang()] }}" class="img-fluid text-center">
-                        </div>
-                        <div class="title-group w-100 text-center py-4">
-                            <h5>
-                                {{ trans('home.company') }}
-                            </h5>
-                            <h1>
-                                {{ $company['name_' . getLang()] }}
-                            </h1>
+                        <div class="d-flex justify-content-center flex-column align-items-center">
+                            <div class="company-img w-50 h-50 d-flex justify-content-center align-items-center mb-4">
+                                <img src="{{ $company->image }}" alt="{{ $company['name_' . getLang()] }}" alt="company 3" class="img-fluid">
+                            </div>
+                            <div class="title-group w-100 text-center py-4">
+                                <h5>
+                                    {{ trans('home.company') }}
+                                </h5>
+                                <h1>
+                                    {{ $company['name_' . getLang()] }}
+                                </h1>
+                            </div>
                         </div>
                     </a>
                 </div>
